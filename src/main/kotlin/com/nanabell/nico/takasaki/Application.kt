8e129fc6d4069f5e82047aca398c999cde75d149
Nanabell <marlon.delfin@gmail.com>
2021-03-25 10:55:04 +0100

    Add Startup Logging info for Metrics Collection Status

--- src/main/kotlin/com/nanabell/nico/takasaki/Application.kt
+++ src/main/kotlin/com/nanabell/nico/takasaki/Application.kt
@@ -1,13 +1,17 @@
 package com.nanabell.nico.takasaki
 
+import io.micronaut.context.annotation.Context
 import io.micronaut.runtime.Micronaut.build
+import io.micronaut.runtime.event.ApplicationStartupEvent
+import io.micronaut.runtime.event.annotation.EventListener
 import io.swagger.v3.oas.annotations.OpenAPIDefinition
 import io.swagger.v3.oas.annotations.info.Contact
 import io.swagger.v3.oas.annotations.info.Info
 import io.swagger.v3.oas.annotations.info.License
+import org.slf4j.LoggerFactory
 import java.lang.management.ManagementFactory
 
-
+@Context
 @OpenAPIDefinition(
     info = Info(
         title = "Yu Takasaki",
@@ -16,32 +20,47 @@ import java.lang.management.ManagementFactory
         license = License(name = "MIT")
     )
 )
-object Application {
+class Application {
 
-    @JvmStatic
-    fun main(args: Array<String>) {
-        val builder = build()
-            .args(*args)
-            .packages("com.nanabell.nico.takasaki")
-            .defaultEnvironments("prod")
+    private val logger = LoggerFactory.getLogger(Application::class.java)
 
-        if (isDevEnv())
-            builder.environments("dev")
+    @EventListener
+    fun init(event: ApplicationStartupEvent) {
+        val environment = event.source.environment
 
-        builder.start()
+        environment.getProperty("micronaut.metrics.enabled", Boolean::class.java, false)!!.let {
+            when (it) {
+                true -> logger.info("Metrics Reporting is enabled, Please ensure you have configured a Metrics export!")
+                false -> logger.warn("Metrics Reporting is disabled! No Metrics will be collected!")
+            }
+        }
     }
 
+    companion object {
 
-    private fun isDevEnv(): Boolean {
-        for (inputArgument in ManagementFactory.getRuntimeMXBean().inputArguments) {
-            if (inputArgument.startsWith("-javaagent")
-                || inputArgument.startsWith("-agentpath")
-                || inputArgument.startsWith("-agentlib")
-            )
-                return true
+        @JvmStatic
+        fun main(args: Array<String>) {
+            val builder = build()
+                .args(*args)
+                .packages("com.nanabell.nico.takasaki")
+                .defaultEnvironments("prod")
+
+            if (isDevEnv())
+                builder.environments("dev")
+
+            builder.start()
         }
 
-        return false
-    }
+        private fun isDevEnv(): Boolean {
+            for (inputArgument in ManagementFactory.getRuntimeMXBean().inputArguments) {
+                if (inputArgument.startsWith("-javaagent")
+                    || inputArgument.startsWith("-agentpath")
+                    || inputArgument.startsWith("-agentlib")
+                )
+                    return true
+            }
 
+            return false
+        }
+    }
 }
