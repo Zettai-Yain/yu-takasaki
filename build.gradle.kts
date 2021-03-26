029cd83e5499edff5486618689826f70f09d60ce
Nanabell <marlon.delfin@gmail.com>
2021-03-27 00:23:55 +0100

    Bump version to 1.2.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
     id("io.micronaut.application") version "1.3.4"
 }
 
-version = "1.1.1"
+version = "1.2.0"
 group = "com.nanabell.nico.discord"
 
 val kotlinVersion = project.properties["kotlinVersion"]
