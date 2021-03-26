ade0cbdb451afcc9df970b79ccb270e582ca1497
Nanabell <marlon.delfin@gmail.com>
2021-03-26 15:46:24 +0100

    Add Presence to MemberCounterMeter

--- src/main/kotlin/com/nanabell/nico/takasaki/metrics/MemberCountMeter.kt
+++ src/main/kotlin/com/nanabell/nico/takasaki/metrics/MemberCountMeter.kt
@@ -8,6 +8,7 @@ import io.micronaut.context.annotation.Requires
 import io.micronaut.core.util.StringUtils
 import io.micronaut.scheduling.annotation.Scheduled
 import net.dv8tion.jda.api.JDA
+import net.dv8tion.jda.api.entities.Guild
 import org.slf4j.LoggerFactory
 import javax.annotation.PostConstruct
 import javax.inject.Singleton
@@ -22,10 +23,17 @@ class MemberCountMeter(
 ) {
 
     private val logger = LoggerFactory.getLogger(MemberCountMeter::class.java)
+    private var metadata: Guild.MetaData = Guild.MetaData(-1, -1, -1, -1)
 
-    private val gauge = Gauge.builder("members", this::getMemberCount)
+    private val memberMeter = Gauge.builder("guild") { metadata.approximateMembers }
         .description("Current Members in the Guild. This value might not be 100% accurate!")
-        .tag("guild", "${config.guild}")
+        .tag("type", "member")
+        .baseUnit("Member")
+        .register(registry)
+
+    private val presenceMeter = Gauge.builder("guild") { metadata.approximatePresences }
+        .description("Current Members in the Guild. This value might not be 100% accurate!")
+        .tag("type", "presence")
         .baseUnit("Member")
         .register(registry)
 
@@ -34,20 +42,16 @@ class MemberCountMeter(
         logger.info("Starting Scheduler for ${this::class.java.simpleName}")
     }
 
-
     @Scheduled(initialDelay = "30s", fixedRate = "5m")
     fun scheduled() {
-        logger.info("Submitted current Member count ${gauge.value()} to MetricsRegistry")
-    }
-
-    private fun getMemberCount(): Int {
+        logger.debug("Refreshing MemberCountMeter")
         val guild = jda.getGuildById(config.guild)
         if (guild == null) {
             logger.error("JDA Failed to retrieve Guild ${config.guild}. Is the Bot Connected to specified Guild?")
-            return -1
+            return
         }
 
-        return guild.memberCount
+        metadata = guild.retrieveMetaData().complete()
+        logger.info("Submitted MemberCount [${presenceMeter.value().toInt()}/${memberMeter.value().toInt()}] to MetricsRegistry")
     }
-
 }
